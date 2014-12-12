74d502b7ece7ddf0b92f4867efc0216f2e5391f2
Andrew Murphy <andrewm1986@gmail.com>
2014-12-12 15:49:55 +0000

    Define regular expressions

--- IronSwift.Compiler.Lexer/Tokens/Token.cs
+++ IronSwift.Compiler.Lexer/Tokens/Token.cs
@@ -3,31 +3,41 @@
     public abstract class Token
     {
         public string LiteralText { get; protected set; }
+        public abstract string RegularExpression { get; }
     }
+
     public class VarKeywordToken : Token
     {
+        public override string RegularExpression => "var";
     }
 
     public class LetKeywordToken : Token
     {
+        public override string RegularExpression => "let";
     }
 
     public class IdentifierToken : Token
     {
+        public override string RegularExpression => "[a-zA-Z][a-zA-Z0-9]*";
     }
 
     public class WhitespaceToken : Token
     {
+        public override string RegularExpression => "[ \t\r\n]+";
     }
 
     public class SingleEqualsToken : Token
     {
+        public override string RegularExpression => "=";
     }
 
     public class ColonKeywordToken : Token
     {
+        public override string RegularExpression => ":";
     }
+
     public class DecimalLiteralToken : Token
     {
+        public override string RegularExpression => "[0-9]+(\\.[0-9]+)?";
     }
 }
