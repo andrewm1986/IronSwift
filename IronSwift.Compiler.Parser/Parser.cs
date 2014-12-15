7fd4aecb588b60e81b3df685d22546eadcabd274
Andrew Murphy <andrewm1986@gmail.com>
2014-12-15 15:47:21 +0000

    Add parser structure

--- IronSwift.Compiler.Parser/Parser.cs
+++ IronSwift.Compiler.Parser/Parser.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using IronSwift.Compiler.Lexer.Tokens;
+using IronSwift.Compiler.Parser.AbstractSyntaxTree;
 
 namespace IronSwift.Compiler.Parser
 {
     public class Parser
     {
+        public IEnumerable<Token> tokens;
+
+        private Parser()
+        {
+
+        }
+
+        public static Parser FromEnumerable(IEnumerable<Token> tokens)
+        {
+            var parser = new Parser {tokens = tokens};
+
+            return parser;
+        }
+
+        public DocumentNode GetTree()
+        {
+            return new DocumentNode();
+        }
     }
 }
